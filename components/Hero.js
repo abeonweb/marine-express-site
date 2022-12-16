46ebdd9d522adc5c3daf1e65cd826c1dd796d431
abeonweb <bmospecific@yahoo.com>
2022-12-16 19:39:01 +0100

    remove react-player

--- components/Hero.js
+++ components/Hero.js
@@ -1,22 +1,11 @@
-// import marineExpressVideo from "../images/marine-express-vid-2.mp4";
-import ReactPlayer from "react-player";
 import styles from "../styles/Home.module.css"
 
 function Hero() {
   return (
     <section className={styles.hero}>
       <div className={styles.overlay} />
-      {/* <ReactPlayer
-        url="../images/marine-express-vid-2.mp4"
-        playing={true}
-        muted
-        loop={true}
-        // width="150%"
-        // height="100%"
-        className={styles.heroVideo}
-      /> */}
       <video autoPlay muted loop className={styles.heroVideo}>
-        <source src="../images/marine-express-vid-2.mp4" type="video/mp4" />
+        <source src="../images/marine-express-vid.mp4" type="video/mp4" />
       </video>
       <div className={styles.heroContainer}>
         <h1 className={styles.heroTitle}>Leading the Way In Business Logistics</h1>
