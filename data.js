d6b5ad4bc2796258cb3f25a88df7ef06efd04ebb
abeonweb <bmospecific@yahoo.com>
2022-12-17 23:35:42 +0100

    link data added

---
+++ data.js
@@ -0,0 +1,72 @@
+const features = [
+    {
+        title: "Clearing",
+        text: "Specialised services we provide as a customs agent to our clients",
+        list: [
+            "Provision of a suitable environment conducive to clear your cargo in order to avoid payment of demurrage",
+            "Adequate information about ship movement with their cargo. That is name of ship agents, arrival and berthing dates of the ship, and the port of discharge",
+            "submission and completion of all necessary shipping / NPA documentation",
+            "Processing of all customs documents to the final relaeasing stage",
+            "Supervision of discharging and loading operations", ,
+            "Supervision and sighting of goods discharged from the carrying vessel to arrange for private  protection so as to prevent pilfering",
+            "Delivery of consignment to the importers premises or warehouse", ,
+            "Providing information about duty payments through any of the designated banks to the importers, in order to avoid unnecessary delays",
+            "Submission of all relevant document to the importe for remittance purposes throught he bank  after the goods have been cleared",
+            "Informing the importer where necessary, through ddetailed report of the consignment condition as at the time of discharge",
+        ]
+
+    },
+    {
+        title: "Forwarding",
+        text: "Our services as a forwarder",
+        list: [
+            "Liase effectively with the exporters on how to register with the Nigerian Export Promotion Council",
+
+            "Process of export Bill of Entry with customs",
+
+            "Booking of space with the shipping company up to final destination",
+
+            "Securing of competitive freight rates",
+
+            "Provision of adequate warehousing if required",
+
+            "Processing of export documents with the relevant port operators i.e Nigeria Ports Authority,  National Maritime Authority, Shipping Companies",
+
+            " Securing all export clearances form the approriate Govenment agencies such as the National Drug Law Enforcement Agency, State Security Services, Ministry of Agriculture etc",
+
+            " Collection of Bill of Lading and other vital documents from shipping companies for immediate deleivery to the exporter",
+
+            " Effective haulage syatem for all cargoes both containarized and palleticed up to the carrying  shipping company terminal",
+        ]
+
+    },
+]
+
+const about = [
+    {
+        title: "Introduction",
+        text: "Marine Express Nigera Limited (MENL), incorporated on the 4th of May 1988 with RC number 112669fully commenced business in Nigeria in June 1988 at 4 Ogunlana Drive, SUrulere hitherto its registered office address.",
+    },
+    {
+        title: "Mission Statement",
+        text: `MENL is determind to set the pace in the clearing and forwarding agency sector by making general importation and exportation an easy and comfortable venture and ensuring a safe and timelydelivery of merchandise,
+         ensuring cost effectiveness without compromising quality of service,and having consideration for the implication of demurrage`,
+    },
+]
+
+const links = [
+    {
+        name: "Home",
+        path: "/"
+    },
+    {
+        name: "About us",
+        path: "/about"
+    },
+    {
+        name: "Contact us",
+        path: "/contact"
+    },
+]
+
+export { features, links }
