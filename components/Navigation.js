95f230ddb7b2f8955a473b26c089aa6041ee8f39
abeonweb <bmospecific@yahoo.com>
2022-12-20 06:38:42 +0100

    navigation update

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,17 +1,48 @@
+import { useEffect } from "react"
 import Link from "next/link"
+import useOpen from "../hooks/useOpen"
 import { links } from "../data"
+import styles from "../styles/Navigation.module.css"
 
 export default function Navigation() {
+    const { open, handleClick } = useOpen()
+
+    useEffect(() => {
+        window.addEventListener("scroll", iScrolled)
+
+        return function () {
+            window.removeEventListener("scroll", iScrolled)
+        }
+    },[])
+
+    function iScrolled() {
+        const header = document.querySelector("#header")
+        const main = document.querySelector("#main")
+        if (window.pageYOffset > (header.clientHeight))
+            main.classList.add("sticky")
+        else
+            main.classList.remove("sticky")
+    }
+
     return (
-        <header>
-            <h1 className="navTitle">MENL</h1>
-            <nav>
-                <ul>
-                    {
-                        links.map(({ name, path }, i) => <li key={i}><Link href={path}>{name}</Link></li>)
-                }
-                </ul>
-            </nav>
+        <header id="header" className={`${styles.header} ${open ? styles.navOpen : null}`} >
+            <div className={styles.navContent}>
+                <div className={styles.navLogo} >
+                    {/* <img src="" className={styles.logoImage} alt="" /> */}
+                    <h1 className={styles.navTitle}>MENL</h1>
+                </div>
+                <div onClick={handleClick} className={styles.menuIcon}>
+                    <span className={styles.hamburger}></span>
+                </div>
+                <nav className={styles.nav}>
+                    <ul className={styles.navList}>
+                        {
+                            links.map(({ name, path }, i) => <li key={i}><Link href={path} className={styles.navLink}>{name}</Link></li>)
+                        }
+                    </ul>
+
+                </nav>
+            </div>
         </header>
     )
 }
