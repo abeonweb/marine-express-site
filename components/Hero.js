fb93b90b4647e49257cad58ba12d32c018d89340
abeonweb <bmospecific@yahoo.com>
2022-12-21 20:43:55 +0100

    updated to  video file

--- components/Hero.js
+++ components/Hero.js
@@ -4,15 +4,17 @@ function Hero() {
   return (
     <section className={styles.hero}>
       <div className={styles.overlay} />
-      <video autoPlay muted loop className={styles.heroVideo}>
-        <source src="../images/marine-express-vid.mp4" type="video/mp4" />
+      <video autoPlay muted loop className={styles.heroVideo} poster="../images/poster.png">
+        <source src="../images/marine-express-intro.mp4" type="video/mp4" />
       </video>
+      {/* <img alt="poster image mobile" className={styles.poster} src="../images/poster.png" /> */}
       <div className={styles.heroContainer}>
         <h1 className={styles.heroTitle}>Leading the Way In Business Logistics</h1>
         <p className={styles.heroBody}>
-          Over 30 years experience in the industry. You can rely on us to get
-          your merchandise where it needs to be.
+          With more than 30 years experience. You can rely on us to get
+          your business where it needs to be.
         </p>
+        <button className={styles.ctaBtn}>Get started</button>
       </div>
     </section>
   );
