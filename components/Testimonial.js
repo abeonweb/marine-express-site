ba4e108a950b775f02bb68ebb1bef959f949a44d
abeonweb <bmospecific@yahoo.com>
2022-12-21 15:30:56 +0100

    added testimonial component

---
+++ components/Testimonial.js
@@ -0,0 +1,17 @@
+import styles from "../styles/Testimonials.module.css"
+
+function Testimonial({ image, text, info }) {
+    return (
+        <article className={styles.testimonial}>
+            <div className={styles.container}>
+                <img className={styles.image} src={image} alt="" />
+                <blockquote className={styles.quote}>
+                    {text}
+                </blockquote>
+            </div>
+            <div className={styles.info}>{info}</div>
+        </article>
+    )
+}
+
+export default Testimonial
