7361a8a57bccff5d059070f5f19428bdec2b8a9d
abeonweb <bmospecific@yahoo.com>
2022-12-21 15:29:55 +0100

    feature component

---
+++ components/Feature.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from '../styles/Features.module.css'
+
+const Feature = ({ title, text, image, alt }) => {
+
+  return (
+    <div className={styles.featureContainer}>
+      <article className={styles.feature}>
+        <div className={styles.imageTextContainer}>
+          <img src={image} alt={alt} className={styles.image} />
+          <div className={styles.textContainer}>
+            <h2 className={styles.title}>{title}</h2>
+            <p className={styles.text}>{text}</p>
+          </div>
+        </div>
+      </article>
+    </div>
+  )
+}
+
+export default Feature
