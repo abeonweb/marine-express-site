248ae38d0b9cefaec90dbd6cc549b68d2435dc68
abeonweb <bmospecific@yahoo.com>
2022-12-31 05:59:03 +0100

    added Head to shared layout to appear on all pages

--- components/SharedLayout.js
+++ components/SharedLayout.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Head from "next/head"
 import Navigation from "./Navigation"
 import Footer from "./Footer"
 
-const SharedLayout = ({children}) => {
+const SharedLayout = ({ children }) => {
     return (
         <>
             <Navigation />
+            <Head>
+                <title>Marine Express Nigeria Limited</title>
+                <meta name="description" content="Licensed clearing and forwarding agent. We handle logistics to and from Nigeria." />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <main id="main">{children}</main>
             <Footer />
         </>
