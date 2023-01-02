d232451c8ffd1f88e19fa112cc78ba0ccddcd046
abeonweb <bmospecific@yahoo.com>
2023-01-02 05:13:14 +0100

    added a relevant image as mobile, poster image

--- components/Hero.js
+++ components/Hero.js
@@ -5,10 +5,10 @@ function Hero() {
   return (
     <section className={styles.hero}>
       <div className={styles.overlay} />
-      <video autoPlay muted loop className={styles.heroVideo} poster="../images/closing-deal.webp">
+      <video autoPlay muted loop className={styles.heroVideo} poster="../images/ships.webp">
         <source src="../images/marine-express-intro.mp4" type="video/mp4" />
       </video>
-      {/* <img alt="poster image mobile" className={styles.poster} src="../images/closing-deal.webp" /> */}
+      {/* <img alt="poster image mobile" className={styles.poster} src="../images/ships.webp" /> */}
       <div className={styles.heroContainer}>
         <h1 className={styles.heroTitle}>Leading the Way In Business Logistics</h1>
         <p className={styles.heroBody}>
