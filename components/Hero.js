6ecfde2b000ec92449e6bb6cee9366df43808699
abeonweb <bmospecific@yahoo.com>
2023-02-14 08:32:48 +0100

    changed link to contact page

--- components/Hero.js
+++ components/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
       <video autoPlay muted loop className={styles.heroVideo} poster="../images/ships.webp">
         <source src="../images/marine-express-intro.mp4" type="video/mp4" />
       </video>
-      {/* <img alt="poster image mobile" className={styles.poster} src="../images/ships.webp" /> */}
+      {/* <Image width={1000} alt="poster image mobile" className={styles.poster} src="../images/ships.webp" /> */}
       <div className={styles.heroContainer}>
         <h1 className={styles.heroTitle}>Marine Express Logistics</h1>
         <h2 className={styles.heroSubTitle}>Licensed Customs Agent.</h2>
@@ -17,7 +17,7 @@ function Hero() {
           With more than 30 years experience. You can rely on us to get
           your business where it needs to be.
         </p>
-        <Link href={"/services"} className={styles.ctaBtn}>Get started</Link>
+        <Link href={"/contact"} className={styles.ctaBtn}>Get started</Link>
       </div>
     </section>
   );
