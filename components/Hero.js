44c6027fb33452087c5918b8c75601ce00fdb99b
abeonweb <bmospecific@yahoo.com>
2023-02-15 19:01:35 +0100

    next/image added

--- components/Hero.js
+++ components/Hero.js
@@ -1,3 +1,4 @@
+// import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css"
 
@@ -8,14 +9,13 @@ function Hero() {
       <video autoPlay muted loop className={styles.heroVideo} poster="../images/ships.webp">
         <source src="../images/marine-express-intro.mp4" type="video/mp4" />
       </video>
-      {/* <Image width={1000} alt="poster image mobile" className={styles.poster} src="../images/ships.webp" /> */}
+      {/* <Image width={1000} height={500} alt="poster image mobile" className={styles.poster} src="/images/ships.webp" /> */}
       <div className={styles.heroContainer}>
         <h1 className={styles.heroTitle}>Marine Express Logistics</h1>
         <h2 className={styles.heroSubTitle}>Licensed Customs Agent.</h2>
         <p className={styles.heroBody}>
           Leading the Way In Business Logistics.
-          With more than 30 years experience. You can rely on us to get
-          your business where it needs to be.
+          With over <span className={styles.highlight}>30 years experience.</span> You can rely on us.
         </p>
         <Link href={"/contact"} className={styles.ctaBtn}>Get started</Link>
       </div>
