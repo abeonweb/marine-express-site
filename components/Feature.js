09f7498c1025e8b13681e3436c261b29afe2a7b1
abeonweb <bmospecific@yahoo.com>
2024-01-29 21:09:05 +0100

    update feature card

--- components/Feature.js
+++ components/Feature.js
@@ -5,19 +5,14 @@ import Image from "next/image"
 const Feature = ({ title, text, image, alt }) => {
 
   return (
-    <div className={styles.featureContainer}>
-      <article className={styles.feature}>
-        <div className={styles.imageTextContainer}>
-          <div className={styles.imageContainer}>
-            <Image width={400} height={500} src={image} alt={alt} className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <h2 className={styles.title}>{title}</h2>
-            <p className={styles.captionText}>{text}</p>
-          </div>
-        </div>
-      </article>
-    </div>
+    <article className={styles.feature}>
+      <div className={styles.cardOverlay}/>
+      <Image width={400} height={500} src={image} alt={alt} className={styles.featureImage} />
+      <div className={styles.textContainer}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.cardText}>{text}</p>
+      </div>
+    </article>
   )
 }
 
