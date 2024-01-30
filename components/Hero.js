a35bb720c0f63fbdfcb9c565d04bb7d668267ebc
abeonweb <bmospecific@yahoo.com>
2024-01-30 22:32:13 +0100

    and pause/play on hero area click

--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,25 @@
-// import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Home.module.css"
 
 function Hero() {
+  const videoRef = useRef(null);
+
+  const [videoControl, setVideoControl] = useState(true);
+
+  useEffect(() => {
+    videoControl? videoRef.current.play(): videoRef.current.pause();
+  
+    
+  }, [videoControl])
+  
   return (
     <section className={styles.hero}>
       <div className={styles.overlay} />
-      <video autoPlay muted loop className={styles.heroVideo} poster="/images/poster.webp">
+      <video ref={videoRef} autoPlay muted loop className={styles.heroVideo} poster="/images/poster.webp">
         <source src="/images/marine-express-intro.mp4" type="video/mp4" />
       </video>
-      <div className={styles.heroContainer}>
+      <div className={styles.heroContainer} onClick={()=>setVideoControl(prev => !prev)}>
         <h1 className={styles.heroTitle}>Marine Express Logistics</h1>
         <h2 className={styles.heroSubTitle}>Licensed Customs Agent.</h2>
         <p className={styles.heroBody}>
