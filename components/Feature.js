fa613f06111fc4a22f1372b7c65ff3d961b79abf
abeonweb <bmospecific@yahoo.com>
2024-01-30 22:31:24 +0100

    improved feature on large screen devices

--- components/Feature.js
+++ components/Feature.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import styles from '../styles/Features.module.css'
-import Image from "next/image"
+import React from 'react';
+import styles from '../styles/Features.module.css';
+import Image from "next/image";
+import Link from "next/link";
 
 const Feature = ({ title, text, image, alt }) => {
 
   return (
     <section className={styles.featureContainer}>
-      <article className={styles.featureContent}>{text}</article>
+      <article className={styles.featureContentLargeScreen}>
+        <p className={styles.featureLargeText}>
+          {text}
+        </p>
+        <Link className={styles.featureContactBtn} href={'/contact'}>Contact us</Link>
+      </article>
       <article className={styles.feature}>
-        <div className={styles.cardOverlay} />
-        <Image width={400} height={500} src={image} alt={alt} className={styles.featureImage} />
+        <div className={styles.featureImageContainer}>
+          <div className={styles.cardOverlay} />
+          <Image width={400} height={500} src={image} alt={alt} className={styles.featureImage} />
+        </div>
         <div className={styles.textContainer}>
           <h2 className={styles.title}>{title}</h2>
           <p className={styles.cardText}>{text}</p>
